d6e497efbecf6dd15dba1dcde2966de4b6d61f45
egoel5 <egoel13@gmail.com>
2023-10-10 19:47:55 -0400

    Initial commit

---
+++ app/src/main/java/com/example/c323_project6/NotesAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.c323_project6
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter : RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
