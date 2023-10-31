303babd097440c1a5594e9f03044da542cefc4b4
egoel5 <egoel13@gmail.com>
2023-10-31 17:42:17 -0400

    Finish Project 7

--- app/src/main/java/com/example/c323_project6/UserScreen.kt
+++ app/src/main/java/com/example/c323_project6/UserScreen.kt
@@ -10,11 +10,6 @@ import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import com.example.c323_project6.databinding.FragmentUserScreenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [UserScreen.newInstance] factory method to
